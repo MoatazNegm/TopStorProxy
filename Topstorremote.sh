468ca7d167df2475119fcfbd9b16fce50276f1fe
MezoAmazon <MezoAmazon@gmail.com>
2015-06-27 20:18:40 +0200

    I think we finished the proxy will try the web now

--- Topstorremote.sh
+++ Topstorremote.sh
@@ -15,55 +15,62 @@ while true; do
  nc -l 2234 | gunzip | openssl enc -d -aes-256-cbc -a -A -k SuperSecretPWD  > /tmp/msgremotefile  &
  read line < /tmp/msgremotefile;
  echo $line > /TopStor/tmplineremote
- stamp=`echo $line | awk '{print $2}'`
- request=`echo $line | awk '{print $1}'`
- receiver=`echo $line | awk '{print $3}'`
- license=`echo $line | awk '{print $4}'`;
- fromtype=`echo $line | awk '{print $5}'`
- pp=`echo $line | awk '{print $7}'`
- passphrase=`echo $line | awk '{print $8}'`
- if [[ $stamp != $oldstamp[$pp] ]]; then 
-  if [[ $fromtype == 'ProxysendReInit.sh' ]];
-  then
-   oldstamp[$pp]=$stamp;
-   searchs=${request}' '${receiver}
-   isreceivero=`cat receiver.txt | grep -w "$searchs" | grep -w "$passphrase" `;
-   isreceiveron=`echo $isreceivero | wc -c`;
-   isreceiveron=$((isreceiveron+0));
-   if [[ $isreceiveron -ge 3 ]];
+ linecomplete=` echo $line | tr -dc " " | wc -c `;
+ if [[ $linecomplete -ge 7 ]];
+ then
+  stamp=`echo $line | awk '{print $2}'`
+  request=`echo $line | awk '{print $1}'`
+  receiver=`echo $line | awk '{print $3}'`
+  license=`echo $line | awk '{print $4}'`;
+  fromtype=`echo $line | awk '{print $5}'`;
+  pp=`echo $line | awk '{print $7}'`;
+  passphrase=`echo $line | awk '{print $8}'`;
+  if [[ $stamp != $oldstamp[$pp] ]]; then 
+   if [[ $fromtype == 'ProxysendReInit.sh' ]];
    then
-    leftlines=`cat receiver.txt | grep -v -w "$searchs" `
-    echo $leftlines > receiver.txt
-    echo $isreceivero $pp >> receiver.txt 
-    inoutpp=`echo $isreceivero | awk '{print $5}'`;
-    reqparam=`echo $line | awk '{$1=$2=$3=$4=""; print substr($0,5) }'`;
-    instr=`echo $reqparam | awk '{print $1 }'`;
-    oper=`echo $reqparam | awk '{$1=""; print substr($0,2) }'`;
-    ./$instr $stamp $oper $inoutpp > tmpl &;
-    echo ./$instr $stamp $oper $inoutpp > tmpl ;
-   else
-    echo $stamp waiting receiver > out &
+    oldstamp[$pp]=$stamp;
+    echo $oldstamp[$pp] is $stamp >> txt/proxystamp;
+    searchs=${request}' '${receiver}
+    isreceivero=`cat receiver.txt | grep -w "$searchs" | grep -w "$passphrase" | awk -F"$license"  '{print $1}'`;
+    isreceiveron=`echo $isreceivero | wc -c`;
+    isreceiveron=$((isreceiveron+0));
+    if [[ $isreceiveron -ge 3 ]];
+    then
+     leftlines=`cat receiver.txt | grep -v -w "$searchs" `
+     echo $leftlines > receiver.txt
+     echo $isreceivero $license $pp $stamp >> receiver.txt 
+     inoutpp=`echo $isreceivero | awk '{print $5}'`;
+     reqparam=`echo $line | awk '{$1=$2=$3=$4=""; print substr($0,5) }'`;
+     instr=`echo $reqparam | awk '{print $1 }'`;
+     oper=`echo $reqparam | awk '{$1=""; print substr($0,2) }'`;
+     ./$instr $stamp ${receiver} $pp $inoutpp > tmpl &;
+     echo ./$instr $stamp ${receiver} $pp $inoutpp > tmpl &;
+    else
+     echo $stamp waiting receiver > out &
+    fi
    fi
   fi
+  if [[ $stamp != $oldstamp[$pp] ]]; then 
+   if [[ $fromtype == 'ProxyrcvReInit.sh' ]];
+   then
+    oldstamp[$pp]=$stamp;
+    echo old is $oldstamp[$pp] is $stamp >> txt/proxystamp;
+    licensed=`cat license.txt | grep -w "$license" | wc -c `;
+    echo $licensed >> tmplineremote
+    if [[ $licensed -ge 4 ]];
+    then
+     searchs=${receiver}' '${request}
+     receivers=` cat receiver.txt | grep -v -w "$searchs"`;
+     echo $receivers > receiver.txt;
+     thissender=` cat receiver.txt | grep -w "$searchs" | awk -F"$license" '{$1=""; print substr($0,2)}'`;
+     echo $receiver $request $stamp $passphrase $pp $license $thissender >> receiver.txt
+     echo $stamp waiting sender > out &
+    else
+     echo $stamp Not_Authorized > out &
+    fi
+   fi
+  fi;
  fi
- if [[ $stamp != $oldstamp[$pp] ]]; then 
-  if [[ $fromtype == 'ProxyrcvReInit.sh' ]];
-  then
-   oldstamp[$pp]=$stamp;
-   licensed=`cat license.txt | grep -w "$license" | wc -c `;
-   echo $licensed >> tmplineremote
-   if [[ $licensed -ge 4 ]];
-   then
-    searchs=${receiver}' '${request}
-    receivers=` cat receiver.txt | grep -v -w "$searchs" `;
-    echo $receivers > receiver.txt;
-    echo $receiver $request $stamp $license $passphrase $pp >> receiver.txt
-    echo $stamp waiting sender > out &
-   else
-    echo $stamp Not_Authorized > out &
-   fi
-  fi
- fi;
 }
 done;
 echo it is dead >/TopStor/txt/statusremote.txt
